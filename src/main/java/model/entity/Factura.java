eec28735325c92fb1f07a78c429a5bbf038e3071
CandelaBasaez <candelabasaez@gmail.com>
2024-06-09 00:00:16 -0300

    Update Factura.java

--- src/main/java/model/entity/Factura.java
+++ src/main/java/model/entity/Factura.java
@@ -1,6 +1,10 @@
 package model.entity;
 
 public class Factura {
+    private int nroFactura;
+    private int dni;
+
+    //! QUEDA AGREGAR LAS COSAS QUE FALTAN Y USAR LOS ANNOTATIONS NECESARIOS
 
 
 
