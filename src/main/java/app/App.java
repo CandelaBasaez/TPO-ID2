27ccd51b52e71dd438a4de83af443717b9855d9b
CandelaBasaez <candelabasaez@gmail.com>
2024-06-10 18:22:35 -0300

    update

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,6 +2,7 @@ package app;
 
 import funciones.funcionesUsuario;
 import model.entity.Usuario;
+import java.util.Scanner;
 
 public class App {
 
@@ -13,33 +14,84 @@ public class App {
         System.out.println("1. Seleccionar Usuario");
         System.out.println("2. Crear usuario");
         System.out.println("3. Ver usuarios por categorias");
-        System.out.println("4. Crear usuario");
-        System.out.println("5. Crear usuario");
+        System.out.println("4. Asignar usuario a una categoria");
+        System.out.println("5. Ver Catalogo");
+        System.out.println("6. Modificar Catalogo");
+        System.out.println("7. Realizar pedido");
+        System.out.println("8. Pagar factura");
+        System.out.println("9. Ver Registro de Facturas");
+        System.out.println("10. Ver Registro de Pagos");
         System.out.println("-1. Salir del sistema");
 
-        int respuesta = 2;
-        //! INGRESA OPCION
+        Scanner opc = new Scanner(System.in);
+        int opcion = opc.nextInt();
 
-        int dni = 0;
-
-        while (respuesta != -1){
-            if (respuesta == 1){
-                //! INGRESA DNI
-
+        while (opcion != -1){
+            if (opcion == 1) {
+                System.out.println("Ingrese el dni del usuario que desea buscar:");
+                Scanner documento = new Scanner(System.in);
+                int dni = documento.nextInt();
                 Usuario usuario = Usuario.cargarDesdeRedis(dni);
 
-            } else if (respuesta == 2) {
-                //! INGRESA DATOS DE USUARIO NUEVO
-                int dni = null;
-                String nombre = null;
-                String apellido = null;
-                String condIVA = null;
-                String direccion = null;
-                int tiempo = null;
+            } else if (opcion == 2) {
+                Scanner document = new Scanner(System.in);
+                Scanner name = new Scanner(System.in);
+                Scanner surname = new Scanner(System.in);
+                Scanner address = new Scanner(System.in);
+                Scanner condIva = new Scanner(System.in);
+                int dni = document.nextInt();
+                String nombre = name.nextLine();
+                String apellido = surname.nextLine();
+                String direccion = address.nextLine();
+                String condIVA = condIva.nextLine();
 
                 Usuario usuario = funcionesUsuario.crearUsuario(dni, nombre, apellido, condIVA, direccion);
 
-            }else System.out.println("La entrada no es valida.");
+            }else if (opcion == 3) {
+                System.out.println("Ingrese el número correspondiente a la categoria que desea visualizar:");
+                System.out.println("1. Todos los usuarios ordenados desde mayor a menor tiempo de conexion");
+                System.out.println("2. Todos los usuarios de categoria TOP");
+                System.out.println("3. Todos los usuarios de categoria MEDIUM");
+                System.out.println("4. Todos los usuarios de categoria LOW");
+
+                Scanner cat = new Scanner(System.in);
+                int categoria = cat.nextInt();
+
+                if (categoria == 1){
+
+                }else if (categoria == 2){
+
+                }else if (categoria == 3){
+
+                }else if (categoria == 4){
+
+                }else{
+                    System.out.println("Se ha ingresado un dato erroneo");
+                    //VER QUE HAGO ACA
+                }
+
+            }else if (opcion == 4) {
+                System.out.println("Ingrese el DNI del usuario al que le quiere asignar una categroia:");
+                Scanner doc = new Scanner(System.in);
+                int documento = doc.nextInt();
+                funcionesUsuario.asignarCategoriaUsuario(documento);
+                
+            }else if (opcion == 5) {
+
+            }else if (opcion == 6) {
+
+            }else if (opcion == 7) {
+
+            }else if (opcion == 8) {
+
+            }else if (opcion == 9) {
+
+            }else if (opcion == 10) {
+
+            }else{
+                System.out.println("Se ingreso un dato erroneo");
+                //VER QUE HAGO ACA
+            }
         }
 
     }
--- src/main/java/funciones/funcionesUsuario.java
+++ src/main/java/funciones/funcionesUsuario.java
@@ -2,10 +2,14 @@ package funciones;
 
 import model.entity.Usuario;
 import org.neo4j.driver.*;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class funcionesUsuario {
-    
-    public static Usuario crearUsuario(int dni, String nombre, String apellido, String condIVA, String direccion){
+
+    public static Usuario crearUsuario(int dni, String nombre, String apellido, String condIVA, String direccion) {
         Usuario usuario = new Usuario();
         usuario.setDni(dni);
         usuario.setNombre(nombre);
@@ -15,16 +19,41 @@ public class funcionesUsuario {
 
         usuario.guardarEnRedis();
 
-        usuario.agregarCategoria(tiempo,dni);
-
         try (Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "contraCande"))) {
             try (Session session = driver.session()) {
-                Result i=session.run("MATCH (n:Carrito) RETURN COUNT(n)");
-                session.run("CREATE (c" + i+1 + ":Carrito{userDNI:" + dni + "})");
+                Result i = session.run("MATCH (n:Carrito) RETURN COUNT(n)");
+                session.run("CREATE (c" + i + 1 + ":Carrito{userDNI:" + dni + "})");
             }
         }
 
         return usuario;
     }
 
+    public static void mostarUsuariosOrdenados() {
+        try (Jedis jedis = new Jedis("localhost", 6379)) {
+            jedis.zrevrange("categorias", 0, -1);
+        }
+    }
+    public static void mostrarUsuariosTOP(){
+        try (Jedis jedis = new Jedis("localhost", 6379)) {
+            jedis.zrangeByScore("categorias", Double.parseDouble("(240"), Double.parseDouble("inf"));
+        }
+    }
+    public static void mostrarUsuariosMEDIUM(){
+        try (Jedis jedis = new Jedis("localhost", 6379)) {
+            jedis.zrangeByScore("categorias",120,240);
+        }
+    }
+    public static void mostrarUsuariosLOW(){
+        try (Jedis jedis = new Jedis("localhost", 6379)) {
+            jedis.zrangeByScore("categorias",0, Double.parseDouble("(120"));
+        }
+    }
+    public static void asignarCategoriaUsuario(int dni){
+        Scanner time = new Scanner(System.in);
+        int tiempo = time.nextInt();
+        try (Jedis jedis = new Jedis("localhost", 6379)) {
+            jedis.zadd("categorias",tiempo, String.valueOf(dni));
+        }
+    }
 }
